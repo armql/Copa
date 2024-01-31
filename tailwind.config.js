a78c57667a18a41e08c93ec152a3124aab7c2a39
am <arlindmaliqi28@gmail.com>
2024-01-31 13:40:12 +0100

    props

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*. {js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        libre: ["Libre Franklin", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
