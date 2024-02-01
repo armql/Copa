dbd6040c16e31e6fa3115a7680a3e3daf9ace0a0
am <arlindmaliqi28@gmail.com>
2024-02-01 16:24:43 +0100

    nav data

---
+++ src/data/NavData.js
@@ -0,0 +1,9 @@
+const NavData = [
+  { id: 1, name: "Produce", to: "produce" },
+  { id: 2, name: "About", to: "about" },
+  { id: 3, name: "Recipes", to: "recipes" },
+  { id: 4, name: "Stories", to: "stories" },
+  { id: 5, name: "Join us", to: "join-us" },
+];
+
+export { NavData };
