8fd4abef01ed06aace0d29e5e89bc8a617550743
am <arlindmaliqi28@gmail.com>
2024-02-01 16:24:32 +0100

    imports

--- src/router/imports/global-import.js
+++ src/router/imports/global-import.js
@@ -1,5 +1,10 @@
 import { lazy } from "react";
 
 const Home = lazy(() => import("../../views/Home"));
+const Produce = lazy(() => import("../../views/Produce"));
+const About = lazy(() => import("../../views/About"));
+const Recipes = lazy(() => import("../../views/Recipes"));
+const Stories = lazy(() => import("../../views/Stories"));
+const JoinUs = lazy(() => import("../../views/JoinUs"));
 import MainLayout from "../../layout/MainLayout";
-export { MainLayout, Home };
+export { MainLayout, Home, Produce, About, Recipes, Stories, JoinUs };
