dd908910f8b29aae4acc36a959cef82063b05383
am <arlindmaliqi28@gmail.com>
2024-02-04 15:14:48 +0100

    more data

---
+++ src/data/ShortcutData.js
@@ -0,0 +1,14 @@
+const ShortcutData = [
+  { id: 1, name: "Produce", to: "produce" },
+  { id: 2, name: "About us", to: "about" },
+  { id: 3, name: "Recipes", to: "recipes" },
+  { id: 4, name: "Stories", to: "stories" },
+  { id: 5, name: "Find in Store", to: "find" },
+  { id: 6, name: "Contact", to: "contact" },
+  { id: 7, name: "Leadership", to: "team" },
+  { id: 8, name: "Terms of Service", to: "tos" },
+  { id: 9, name: "Privacy Policy", to: "privacy" },
+  { id: 10, name: "Support", to: "support" },
+];
+
+export { ShortcutData };
