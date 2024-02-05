f1bb75a13c21283b40dffd18e7a8101967c0e563
am <arlindmaliqi28@gmail.com>
2024-02-06 00:13:08 +0100

    more core fixes

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,24 @@
 import { Fragment } from "react";
-import HeroSection from "../components/HeroSection";
-import InfoSection from "../components/InfoSection";
-import ProduceSection from "../components/ProduceSection";
-import ProcessSection from "../components/ProcessSection";
-import ArticleSection from "../components/ArticleSection";
-import NewsletterSection from "../components/NewsletterSection";
+import HeroSection from "../components/Home/HeroSection";
+import InfoSection from "../components/Home/InfoSection";
+import ProduceSection from "../components/Home/ProduceSection";
+import ProcessSection from "../components/Home/ProcessSection";
+import ArticleSection from "../components/Home/ArticleSection";
+import NewsletterSection from "../components/Home/NewsletterSection";
 
 export default function Home() {
   return (
     <Fragment>
       <title>Bowery | Home</title>
-      <HeroSection />
-      <ProduceSection />
-      <ProcessSection />
-      <InfoSection />
-      <ArticleSection />
-      <NewsletterSection />
+      <main className="overflow-hidden rounded-b-[60px] shadow-xl">
+        <HeroSection />
+        <ProduceSection />
+        <ProcessSection />
+        <InfoSection />
+        <ArticleSection />
+        <NewsletterSection />
+        <div className="h-24 border-blue-200 bg-stone-100" />
+      </main>
     </Fragment>
   );
 }
