7d86287c159251502d839aed83fe98e02ca77593
am <arlindmaliqi28@gmail.com>
2024-02-19 23:19:44 +0100

    recipe core

---
+++ src/components/Recipes/RecipeDisplay.jsx
@@ -0,0 +1,177 @@
+export default function RecipeDisplay() {
+  return (
+    <section className="flex h-full w-full flex-col gap-12 px-2 py-12 lg:px-12 xl:px-48">
+      <div>filters</div>
+      <div className="grid h-full w-full grid-cols-3 items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-6">
+          <div className="group h-[300px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2584-scaled-e1702416048821-1024x768.jpeg"
+                alt="idk"
+                className="h-[220px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+          <div className="group h-[400px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[320px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+          <div className="group h-[450px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[370px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-6">
+          <div className="group h-[250px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[170px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+          <div className="group h-[400px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[320px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+          <div className="group h-[500px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[420px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-6">
+          <div className="group h-[350px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2477-scaled-e1702414883837-1024x768.jpeg"
+                alt="idk"
+                className="h-[270px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+          <div className="group h-[450px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[370px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+          <div className="group h-[350px] w-[140px] cursor-pointer sm:w-[180px] md:w-[240px] lg:w-[300px] xl:w-[400px]">
+            <div className="relative">
+              <img
+                src="https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg"
+                alt="idk"
+                className="h-[270px] w-full rounded-xl object-cover"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-0 rounded-xl transition-all duration-700 group-hover:bg-black group-hover:bg-opacity-20" />
+            </div>
+            <div className="flex h-[80px] flex-col items-center justify-start gap-1 px-4 pt-0.5">
+              <h1 className="font-libre text-xs font-medium text-green-950 sm:text-sm md:text-base lg:text-lg xl:text-xl">
+                Basil Cosmo Punch Bowl with Basil Ice Wreath
+              </h1>
+              <p className="w-full font-mono text-[10px] uppercase tracking-wide sm:text-xs md:text-xs lg:text-sm xl:text-sm">
+                Bewerages
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
