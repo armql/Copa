47b3b99c0b97d8fa7d60aa02334207b2239707c9
am <arlindmaliqi28@gmail.com>
2024-02-19 23:18:39 +0100

    better sections

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,15 +1,20 @@
-import { Fragment } from "react";
 import HeroSection from "../components/About/HeroSection";
 import CardSection from "../components/About/CardSection";
 import TeamSection from "../components/About/TeamSection";
+import BrandSection from "../components/About/BrandSection";
+import NewsletterSection from "../components/Home/NewsletterSection";
+import CareerCard from "../components/About/CareerCard";
 
 export default function About() {
   return (
-    <Fragment>
+    <div className="overflow-hidden rounded-b-[64px]">
       <title>Bowery | About</title>
       <HeroSection />
       <CardSection />
       <TeamSection />
-    </Fragment>
+      <BrandSection />
+      <NewsletterSection />
+      <CareerCard />
+    </div>
   );
 }
--- src/views/Recipes.jsx
+++ src/views/Recipes.jsx
@@ -1,8 +1,12 @@
+import IntroSection from "../components/Recipes/IntroSection";
+import RecipeDisplay from "../components/Recipes/RecipeDisplay";
+
 export default function Recipes() {
   return (
-    <section className="w-screen h-screen">
+    <main className="relative overflow-hidden rounded-b-[60px] bg-[#f1f9dc] shadow-xl">
       <title>Bowery | Recipes</title>
-      <h1>Recipes</h1>
-    </section>
+      <IntroSection />
+      <RecipeDisplay />
+    </main>
   );
 }
