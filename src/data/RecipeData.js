0d515aeb2935c0395e62d08d62fd5f53871a8608
am <arlindmaliqi28@gmail.com>
2024-02-19 23:18:48 +0100

    more data

---
+++ src/data/RecipeData.js
@@ -0,0 +1,217 @@
+const RecipeData = [
+  {
+    id: "01",
+    category: "Beverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Basil Cosmo Punch Bowl with Basil Ice Wreath",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/12/IMG_2627-scaled-e1702414934686-1024x768.jpeg",
+  },
+  {
+    id: "02",
+    category: "Beverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Bowery Basil Blackberry 'Mule' Mocktail",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowery-Basil-Blackberry-Mule-Mocktail.jpg",
+  },
+  {
+    id: "03",
+    category: "Beverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "La Luna Verde Cocktail",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/La-Luna-Verde-Cocktail-768x1024.jpg",
+  },
+  {
+    id: "04",
+    category: "Beverages",
+    product: "Basil",
+    timespan: "1h",
+    name: "Basil Berry Rum Pops",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/07/View-recent-photos-scaled-e1688575817431-1024x582.jpeg",
+  },
+  {
+    id: "05",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Grand Basil Aioli",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/12/IMG_2504-scaled-e1702414741851-1024x768.jpeg",
+  },
+  {
+    id: "06",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Bowery Cheese Ball",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/12/IMG_2604-scaled-e1702414692677-1024x768.jpeg",
+  },
+  {
+    id: "07",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Green Goddess Dressing with Bowery Basil",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Green-Goddess-Dressing-With-Bowery-Basil.jpg",
+  },
+  {
+    id: "08",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Bowery's Basil & Parsley Pesto",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowery8217s-Basil-038-Parsley-Pesto-768x1024.jpg",
+  },
+  {
+    id: "09",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Bowery 'Ranch-ish' Sauce",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowery-8220Ranch-ish8221-Sauce.jpg",
+  },
+  {
+    id: "10",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Bowery's Herbed Tahini Ranch",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowerys-Herbed-Tahini-Ranch.jpg",
+  },
+  {
+    id: "11",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "Creamy Bowery Basil Cashew Dressing",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Creamy-Bowery-Basil-Cashew-Dressing-941x1024.jpg",
+  },
+  {
+    id: "12",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "15min",
+    name: "The Grossy Goddess Dressing",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/The-Grossy-Goddess-Dressing-931x1024.jpg",
+  },
+  {
+    id: "13",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "30min",
+    name: "Bowery Basil Cashew Cream",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowery-Basil-Cashew-Cream.jpg",
+  },
+  {
+    id: "14",
+    category: "Dips&DressingsBeverages",
+    product: "Basil",
+    timespan: "30min",
+    name: "Bowery's Spicy Herby Green Tahini Sauce",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowerys-Spicy-Herby-Green-Tahini-Sauce.jpg",
+  },
+  {
+    id: "15",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "15min",
+    name: "Breakfast Bowl with Bowery Baby Butter, Aragula and Basil",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Breakfast-Bowl-with-Bowery-Baby-Butter-Arugula-and-Basil.jpg",
+  },
+  {
+    id: "16",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "15min",
+    name: "Breakfast Bowl with Bowery Baby Butter, Aragula and Basil",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Breakfast-Bowl-with-Bowery-Baby-Butter-Arugula-and-Basil.jpg",
+  },
+  {
+    id: "17",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "15min",
+    name: "Candied Bowery Basil and Cranberries",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Candied-Bowery-Basil-and-Cranberries-768x1024.jpg",
+  },
+  {
+    id: "18",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "30min",
+    name: "Green Clam Pasta",
+    image: "https://bowery.co/wp-content/uploads/2023/07/IMG_4096-768x1024.png",
+  },
+  {
+    id: "19",
+    category: "Salad",
+    product: "Basil",
+    timespan: "30min",
+    name: "Red White and Green Pasta Salad",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/06/IMG_7876-scaled-e1688148991595-1024x768.jpg",
+  },
+  {
+    id: "20",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "30min",
+    name: "Cauliflower 'Wings' with Bowery's Herbed Tahini Ranch",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Cauliflower-8220Wings8221-with-Bowerys-Herbed-Tahini-Ranch.jpg",
+  },
+  {
+    id: "21",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "30min",
+    name: "Bowery Basil Potato Hash with Fried Eggs",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Bowery-Basil-Potato-Hash-with-Fried-Eggs-941x1024.jpg",
+  },
+  {
+    id: "22",
+    category: "Dishes",
+    product: "Basil",
+    timespan: "1h",
+    name: "Candied Bowery Basil",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/04/Candied-Bowery-Basil.jpg",
+  },
+  {
+    id: "23",
+    category: "Dishes",
+    product: "Spinach",
+    timespan: "1h",
+    name: "Holiday Pinwheels Three Ways",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/12/IMG_2584-scaled-e1702416048821-1024x768.jpeg",
+  },
+  {
+    id: "24",
+    category: "Dishes",
+    product: "Spinach",
+    timespan: "1h",
+    name: "Greek Spinach Artichoke Dip",
+    image:
+      "https://bowery.co/wp-content/uploads/2023/12/IMG_2442-scaled-e1702414806686-1024x768.jpeg",
+  },
+];
